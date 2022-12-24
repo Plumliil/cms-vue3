38b0309404ab8bf6461bbac74a635919adc8321a
plumliil <476453574@qq.com>
2022-12-24 17:00:03 +0800

    feat: introduce global styles

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import { globalRegister } from './global'
+import './assets/css/index.less'
+import 'normalize.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import plRequest from './service'
+
 const app = createApp(App)
 
 // element-plus注册
@@ -11,21 +13,3 @@ app.use(globalRegister)
 app.use(router)
 app.use(store)
 app.mount('#app')
-
-interface DataType {
-  data: any
-  returnCode: string
-  success: boolean
-  status: number
-}
-
-plRequest
-  .request<DataType>({
-    url: '/home/multidata',
-    interceptors: {}
-  })
-  .then((res) => {
-    console.log(res)
-    console.log(res.status)
-    console.log(res.success)
-  })
