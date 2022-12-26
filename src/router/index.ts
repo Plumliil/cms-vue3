00d1a8b6f5e496e4ad1692e697c67fd260646e33
plumliil <476453574@qq.com>
2022-12-26 17:25:43 +0800

    feat: 404 not found

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 import localCache from '../utils/cache'
+import store from '@/store'
+import { mapMenusToRoute } from '@/utils/mapMenus'
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
@@ -8,11 +10,18 @@ const routes: RouteRecordRaw[] = [
   },
   {
     path: '/login',
+    name: 'login',
     component: () => import('@/views/login/login.vue')
   },
   {
     path: '/main',
+    name: 'main',
     component: () => import('@/views/main/main.vue')
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'not-found',
+    component: () => import('@/views/not-found/not-found.vue')
   }
 ]
 
@@ -25,7 +34,11 @@ router.beforeEach((to) => {
   if (to.path !== '/login') {
     const token = localCache.getCache('token')
     if (!token) return '/login'
+    const userMenus = (store.state as any).login.userMenus
+    const routes = mapMenusToRoute(userMenus)
+    routes.forEach((route) => {
+      router.addRoute('main', route)
+    })
   }
 })
-
 export default router
---
+++ src/views/not-found/not-found.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>404NotFound</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped lang="less"></style>
